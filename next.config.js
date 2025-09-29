70dea998f8b2f0058301d9a8cdd81e2a8d0b571b
Harish V P <harishvp2004@gmail.com>
2025-09-29 22:03:00 +0530

    fix - config try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true,
     domains: ["localhost", "happy-sky-02a622900.4.azurestaticapps.net"],
