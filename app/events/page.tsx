0cbb3e62ed12b5e716bc65d1e199cbd90ca82405
SAVITHA N <143273095+N-SAVITHA@users.noreply.github.com>
2025-09-29 19:36:55 +0530

    Updated page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,40 +1,19 @@
-import SingleEvent from "@/components/Event/SingleEvent";
-import eventsData from "@/components/Event/eventData";
-import Breadcrumb from "@/components/Common/Breadcrumb";
-
+// app/events/page.tsx
 import { Metadata } from "next";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import EventClient from "./EventClient";
+import eventsData from "@/components/Event/eventData";
 
 export const metadata: Metadata = {
   title: "Events",
-  description: "List  of all events conducted by SEC IEEE HKN",
-  // other metadata
+  description: "List of all events conducted by SEC IEEE HKN",
 };
 
-const Event = () => {
-    return (
-        <>
-            <Breadcrumb
-                pageName="Events"
-                description=""
-            />
-            <section className="pb-[120px] pt-[120px]">
-                <div className="container">
-                    <div className="-mx-4 flex flex-wrap justify-center">
-                        {eventsData.map((event) => (
-                            <a
-                                href={"/events/" + event.id}
-                                key={event.id}
-                                className="hover:scale-110 w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-
-                            >
-                                <SingleEvent event={event}/>
-                            </a>
-                        ))}
-                    </div>
-                </div>
-            </section>
-        </>
-    );
-};
-
-export default Event;
+export default function EventPage() {
+  return (
+    <>
+      <Breadcrumb pageName="Events" description="" />
+      <EventClient events={eventsData} />
+    </>
+  );
+}
